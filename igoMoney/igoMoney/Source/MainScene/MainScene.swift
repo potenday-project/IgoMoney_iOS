ea3f400188cbeec1e473439bba26d90b13c8f411
미니 <cow970814@naver.com>
2023-09-18 16:23:31 +0900

    config : 폴더 구조 변경

--- igoMoney/igoMoney/Source/MainScene/MainScene.swift
+++ igoMoney/igoMoney/Source/MainScene/MainScene.swift
@@ -6,13 +6,10 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct MainScene: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("I Go Money Scene")
         }
         .padding()
     }
@@ -20,6 +17,6 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        MainScene()
     }
 }
