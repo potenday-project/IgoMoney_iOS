aeaa78f4f4db2ba70c6a88f4ff54f8e2fb178289
미니 <cow970814@naver.com>
2023-09-18 16:04:05 +0900

    feat : start Repo

---
+++ igoMoney/igoMoney/igoMoneyApp.swift
@@ -0,0 +1,17 @@
+//
+//  igoMoneyApp.swift
+//  igoMoney
+//
+//  Copyright (c) 2023 Minii All rights reserved.
+        
+
+import SwiftUI
+
+@main
+struct igoMoneyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
