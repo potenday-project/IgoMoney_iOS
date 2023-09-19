b2660e4492310d956338996772e8b60d0bf3b9f5
미니 <cow970814@naver.com>
2023-09-19 19:26:24 +0900

    refactor : 회원 가입 페이지 뷰 쪼개기

--- igoMoney/igoMoney/Source/AuthScene/SignUpView/SignUpView.swift
+++ igoMoney/igoMoney/Source/AuthScene/SignUpView/SignUpView.swift
@@ -11,123 +11,62 @@ import ComposableArchitecture
 struct SignUpView: View {
     let store: StoreOf<SignUpCore>
     
+    @ViewBuilder
+    private func informationBaseView(with text: String) -> some View {
+        HStack {
+            Text(text)
+            
+            Spacer()
+        }
+    }
+    
+    
+    
     var body: some View {
         VStack(alignment: .leading, spacing: 10) {
-            HStack {
-                Text("약관에 동의해주세요.")
-                    .font(.system(size: 20, weight: .bold))
-                
-                Spacer()
-            }
+            informationBaseView(with: "약관에 동의해주세요.")
+            informationBaseView(with: "필수항목에 대한 약관에 동의해주세요.")
             
-            HStack {
-                Text("필수항목에 대한 약관에 동의해주세요.")
-                    .font(.system(size: 14))
-                
-                Spacer()
-            }
-            
-            VStack(alignment: .leading) {
-                HStack {
-                    WithViewStore(store, observe: { $0 }) { viewStore in
-                        Button {
-                            viewStore.send(.didTapAll)
-                        } label: {
-                            Image(systemName: "checkmark.circle")
-                                .resizable()
-                                .scaledToFit()
-                                .frame(width: 24, height: 24)
-                                .accentColor(
-                                    viewStore.isAgreeAll ? ColorConstants.primary : .gray.opacity(0.3)
-                                )
-                        }
-                    }
-                    
+            WithViewStore(store, observe: { $0 }) { viewStore in
+                CheckButton(isAccentColor: viewStore.isAgreeAll) {
+                    viewStore.send(.didTapAll)
+                } content: {
                     Text("전체 동의")
                         .font(.system(size: 18, weight: .bold))
                 }
-                
+            }
+            
+            CheckButton(isAccentColor: false, isHidden: true, action: { }) {
                 Text("서비스 이용을 위해 아래 약관에 모두 동의합니다.")
                     .font(.system(size: 14, weight: .medium))
             }
-            .padding(.top, 8)
             
             Rectangle()
                 .fill(Color.gray.opacity(0.3))
                 .frame(height: 1)
                 .padding(.vertical, 10)
             
-            HStack {
-                WithViewStore(store, observe: { $0 }) { viewStore in
-                    Button {
-                        viewStore.send(.didTapAgreePrivacy)
-                    } label: {
-                        Image(systemName: "checkmark.circle")
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 24, height: 24)
-                            .accentColor(
-                                viewStore.isAgreePrivacy ? ColorConstants.primary : .gray.opacity(0.3)
-                            )
-                    }
+            WithViewStore(store, observe: { $0 }) { viewStore in
+                PrivacyCheckView(
+                    title: "개인 정보 처리방침",
+                    isAccentColor: viewStore.isAgreePrivacy
+                ) {
+                    viewStore.send(.didTapAgreePrivacy)
+                } viewAction: {
+                    print("Tapped 서비스 이용약관")
                 }
+            }
 
-                Text("필수")
-                    .foregroundColor(.gray)
-                    .font(.system(size: 12, weight: .regular))
-                    .padding(.horizontal, 8)
-                    .padding(.vertical, 4)
-                    .background(Color.gray.opacity(0.3))
-                    .cornerRadius(4)
-                
-                Text("개인 정보 처리 방침")
-                    .font(.system(size: 16, weight: .regular))
-                
-                Spacer()
-                
-                Button("보기") {
-                    print("Tapped 개인 정보 처리 방침"  )
+            WithViewStore(store, observe: { $0 }) { viewStore in
+                PrivacyCheckView(
+                    title: "서비스 이용약관",
+                    isAccentColor: viewStore.isAgreeTerms
+                ) {
+                    viewStore.send(.didTapAgreeTerms)
+                } viewAction: {
+                    print("Tapped 서비스 이용약관")
                 }
-                .font(.system(size: 12, weight: .regular))
-                .foregroundColor(.gray)
             }
-            .padding(.vertical, 8)
-            
-            HStack {
-                WithViewStore(store, observe: { $0 }) { viewStore in
-                    Button {
-                        viewStore.send(.didTapAgreeTerms)
-                    } label: {
-                        Image(systemName: "checkmark.circle")
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 24, height: 24)
-                            .accentColor(
-                                viewStore.isAgreeTerms ? ColorConstants.primary : .gray.opacity(0.3)
-                            )
-                    }
-                }
-                
-                Text("필수")
-                    .foregroundColor(.gray)
-                    .font(.system(size: 12, weight: .regular))
-                    .padding(.horizontal, 8)
-                    .padding(.vertical, 4)
-                    .background(Color.gray.opacity(0.3))
-                    .cornerRadius(4)
-                
-                Text("서비스 이용약관")
-                    .font(.system(size: 16, weight: .regular))
-                
-                Spacer()
-                
-                Button("보기") {
-                    print("Tapped 개인 정보 처리 방침"  )
-                }
-                .font(.system(size: 12, weight: .regular))
-                .foregroundColor(.gray)
-            }
-            .padding(.vertical, 8)
             
             Spacer()
             
@@ -160,6 +99,95 @@ struct SignUpView: View {
         .background(Color.white)
         .clipShape(RoundedRectangle(cornerRadius: 8))
         .edgesIgnoringSafeArea(.bottom)
+    }
+}
+
+struct PrivacyCheckView: View {
+    let title: String
+    let isAccentColor: Bool
+    let action: () -> Void
+    let viewAction: () -> Void
+    
+    init(
+        title: String,
+        isAccentColor: Bool,
+        action: @escaping () -> Void,
+        viewAction: @escaping () -> Void
+    ) {
+        self.title = title
+        self.isAccentColor = isAccentColor
+        self.action = action
+        self.viewAction = viewAction
+    }
+    
+    var body: some View {
+        CheckButton(isAccentColor: isAccentColor) {
+            action()
+        } content: {
+            Text("필수")
+                .foregroundColor(.gray)
+                .font(.system(size: 12, weight: .regular))
+                .padding(.horizontal, 8)
+                .padding(.vertical, 4)
+                .background(Color.gray.opacity(0.3))
+                .cornerRadius(4)
+            
+            Text(title)
+                .font(.system(size: 16, weight: .regular))
+            
+            Spacer()
+            
+            Button("보기") {
+                viewAction()
+            }
+            .font(.system(size: 12, weight: .regular))
+            .foregroundColor(.gray)
+        }
+    }
+    
+}
+
+struct CheckButton<Content: View>: View {
+    let isAccentColor: Bool
+    let isHidden: Bool
+    let action: () -> Void
+    let content: () -> Content
+    
+    @ViewBuilder
+    private func signUpCheckImage() -> some View {
+        Image(systemName: "checkmark.circle")
+            .resizable()
+            .scaledToFit()
+            .frame(width: 24, height: 24)
+    }
+    
+    init(
+        isAccentColor: Bool,
+        isHidden: Bool = false,
+        action: @escaping () -> Void,
+        @ViewBuilder content: @escaping () -> Content
+    ) {
+        self.isAccentColor = isAccentColor
+        self.isHidden = isHidden
+        self.action = action
+        self.content = content
+    }
+    
+    var body: some View {
+        HStack {
+            Button {
+                action()
+            } label: {
+                signUpCheckImage()
+                    .accentColor(
+                        isAccentColor ? ColorConstants.primary : Color.gray.opacity(0.3)
+                    )
+            }
+            .opacity(isHidden ? .zero : 1)
+            .disabled(isHidden)
+            
+            content()
+        }
     }
 }
 
