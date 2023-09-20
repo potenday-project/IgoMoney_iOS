26a105a041fbbfb3bec261d4825fc2ed8103825c
미니 <cow970814@naver.com>
2023-09-21 00:54:26 +0900

    feat : API Client 생성

---
+++ igoMoney/igoMoney/Source/NetworkClient/APIClient.swift
@@ -0,0 +1,20 @@
+//
+//  APIClient.swift
+//  igoMoney
+//
+//  Copyright (c) 2023 Minii All rights reserved.
+
+import ComposableArchitecture
+
+struct APIClient: Networking { }
+
+extension APIClient: DependencyKey {
+    static var liveValue: APIClient = APIClient()
+}
+
+extension DependencyValues {
+    var apiClient: APIClient {
+        get { self[APIClient.self] }
+        set { self[APIClient.self] = newValue }
+    }
+}
