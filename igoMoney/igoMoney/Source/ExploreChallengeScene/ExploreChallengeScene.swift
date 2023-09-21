0abea6462fb63f7b3f21f6d8fedffc3501c673a6
미니 <cow970814@naver.com>
2023-09-21 18:40:04 +0900

    feat : 챌린지 탐색 화면 파일 생성

---
+++ igoMoney/igoMoney/Source/ExploreChallengeScene/ExploreChallengeScene.swift
@@ -0,0 +1,23 @@
+//
+//  ExploreChallengeScene.swift
+//  igoMoney
+//
+//  Copyright (c) 2023 Minii All rights reserved.
+
+import SwiftUI
+
+import ComposableArchitecture
+
+struct ExploreChallengeScene: View {
+    var body: some View {
+        VStack{
+            Text("Example")
+        }
+    }
+}
+
+struct ExploreChallengeScene_Previews: PreviewProvider {
+    static var previews: some View {
+        ExploreChallengeScene()
+    }
+}
