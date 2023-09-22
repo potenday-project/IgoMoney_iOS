53bbb3bb323b0377c4c90879b8620403df532a28
미니 <cow970814@naver.com>
2023-09-22 15:25:42 +0900

    chore : 컨벤션 정리

--- igoMoney/igoMoney/Source/App/igoMoneyApp.swift
+++ igoMoney/igoMoney/Source/App/igoMoneyApp.swift
@@ -11,44 +11,44 @@ import KakaoSDKCommon
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-    func application(
-        _ application: UIApplication,
-        configurationForConnecting connectingSceneSession: UISceneSession,
-        options: UIScene.ConnectionOptions
-    ) -> UISceneConfiguration {
-        return UISceneConfiguration(
-            name: "Default Configuration",
-            sessionRole: connectingSceneSession.role
-        )
-    }
+  func application(
+    _ application: UIApplication,
+    configurationForConnecting connectingSceneSession: UISceneSession,
+    options: UIScene.ConnectionOptions
+  ) -> UISceneConfiguration {
+    return UISceneConfiguration(
+      name: "Default Configuration",
+      sessionRole: connectingSceneSession.role
+    )
+  }
 }
 
 class SceneDelegate: NSObject, UIWindowSceneDelegate {
-    var window: UIWindow?
+  var window: UIWindow?
+  
+  func scene(
+    _ scene: UIScene,
+    willConnectTo session: UISceneSession,
+    options connectionOptions: UIScene.ConnectionOptions
+  ) {
+    guard let windowScene = scene as? UIWindowScene else { return }
+    window = UIWindow(windowScene: windowScene)
+    //        let rootView = AuthScene(
+    //            store: Store(
+    //                initialState : AuthCore.State(),
+    //                reducer: { AuthCore()._printChanges() }
+    //            )
+    //        )
     
-    func scene(
-        _ scene: UIScene,
-        willConnectTo session: UISceneSession,
-        options connectionOptions: UIScene.ConnectionOptions
-    ) {
-        guard let windowScene = scene as? UIWindowScene else { return }
-        window = UIWindow(windowScene: windowScene)
-        //        let rootView = AuthScene(
-        //            store: Store(
-        //                initialState: AuthCore.State(),
-        //                reducer: { AuthCore()._printChanges() }
-        //            )
-        //        )
-        
-        let rootView = MainScene(
-            store: Store(
-                initialState: MainCore.State(),
-                reducer: { MainCore() }
-            )
-        )
-        
-        KakaoSDK.initSDK(appKey: Bundle.main.kakaoNativeKey)
-        window?.rootViewController = UIHostingController(rootView: rootView)
-        window?.makeKeyAndVisible()
-    }
+    let rootView = MainScene(
+      store: Store(
+        initialState: MainCore.State(),
+        reducer: { MainCore() }
+      )
+    )
+    
+    KakaoSDK.initSDK(appKey: Bundle.main.kakaoNativeKey)
+    window?.rootViewController = UIHostingController(rootView: rootView)
+    window?.makeKeyAndVisible()
+  }
 }
