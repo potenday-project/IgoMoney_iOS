b0768bb98dcf5410b637ed9d2d309e0e5ac7f54d
미니 <cow970814@naver.com>
2023-09-25 16:34:18 +0900

    feat : 네트워크 client 타입 함수 구현

--- igoMoney/igoMoney/Source/NetworkClient/APIClient.swift
+++ igoMoney/igoMoney/Source/NetworkClient/APIClient.swift
@@ -4,9 +4,22 @@
 //
 //  Copyright (c) 2023 Minii All rights reserved.
 
+import Foundation
+
 import ComposableArchitecture
 
-struct APIClient: Networking { }
+struct APIClient: Networking {
+  func request<T>(to generator: RequestGenerator) async throws -> T where T : Decodable {
+    do {
+      let request = try generator.generate()
+      let (data, _) = try await URLSession.shared.data(for: request)
+      return try JSONDecoder().decode(T.self, from: data)
+    } catch {
+      print("Error in API Client \(error)")
+      throw error
+    }
+  }
+}
 
 extension APIClient: DependencyKey {
   static var liveValue: APIClient = APIClient()
