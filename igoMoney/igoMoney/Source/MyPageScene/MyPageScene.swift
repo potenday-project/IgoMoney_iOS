c13953f4f2fcb551ada99fcf2257eebacd654f4c
미니 <cow970814@naver.com>
2023-10-01 19:19:33 +0900

    config : 사용자 설정 이동 아이콘 생성

--- igoMoney/igoMoney/Source/MyPageScene/MyPageScene.swift
+++ igoMoney/igoMoney/Source/MyPageScene/MyPageScene.swift
@@ -9,7 +9,27 @@ import SwiftUI
 struct MyPageScene: View {
   var body: some View {
     VStack {
-      Text("My Page Scene")
+      IGONavigationBar {
+        EmptyView()
+      } leftView: {
+        Text("마이페이지")
+          .font(.pretendard(size: 20, weight: .bold))
+      } rightView: {
+        Button {
+          // TODO: - 설정 이동 메서드 구현
+        } label: {
+          Image("icon_gear")
+        }
+      }
+      
+      // TODO: - 사용자 상세 정보 화면 구현
+      Spacer()
     }
+    .padding(.horizontal, 24)
+    .padding(.vertical, 16)
   }
+}
+
+#Preview {
+  MyPageScene()
 }
