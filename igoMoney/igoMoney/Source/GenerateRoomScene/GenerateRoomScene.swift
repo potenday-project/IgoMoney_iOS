302231b849c4ac72b849070825cd1815632cbd78
미니 <cow970814@naver.com>
2023-10-11 16:56:53 +0900

    refactor : 챌린지 생성 화면 인풋폼 변경

--- igoMoney/igoMoney/Source/GenerateRoomScene/GenerateRoomScene.swift
+++ igoMoney/igoMoney/Source/GenerateRoomScene/GenerateRoomScene.swift
@@ -32,113 +32,90 @@ struct GenerateRoomScene: View {
       ScrollView {
         VStack(spacing: 24) {
           // 챌린지 금액 섹션
-          Section {
-            VStack(alignment: .leading, spacing: 12) {
-              HStack {
-                Text("챌린지 금액")
-                  .font(.pretendard(size: 18, weight: .bold))
-                
-                Spacer()
-              }
-              
-              HStack {
-                ForEach(1...5, id: \.self) { index in
-                  Button {
-                    print("Tapped \(index)")
-                  } label: {
-                    Text(index.description + "만원")
-                      .frame(maxWidth: .infinity)
-                      .padding(.vertical, 8)
-                  }
-                  .buttonStyle(.plain)
-                  .background(ColorConstants.primary7)
-                  .overlay(
-                    RoundedRectangle(cornerRadius: 4)
-                      .stroke(ColorConstants.primary, lineWidth: 1)
-                  )
+          IGOInputForm {
+            Text("챌린지 금액")
+              .font(.pretendard(size: 18, weight: .bold))
+          } content: {
+            HStack {
+              ForEach(1...5, id: \.self) { index in
+                Button {
+                  print("Tapped \(index)")
+                } label: {
+                  Text(index.description + "만원")
+                    .frame(maxWidth: .infinity)
+                    .padding(.vertical, 8)
                 }
+                .buttonStyle(.plain)
+                .background(ColorConstants.primary7)
+                .overlay(
+                  RoundedRectangle(cornerRadius: 4)
+                    .stroke(ColorConstants.primary, lineWidth: 1)
+                )
               }
             }
           }
           
-          // 챌린지 주제 섹션
-          Section {
-            VStack {
-              HStack {
-                Text("챌린지 주제")
-                  .font(.pretendard(size: 18, weight: .bold))
-                
-                Spacer()
-              }
-              
-              LazyVGrid(columns: Array(repeating: .init(), count: 3), spacing: 16) {
-                ForEach(1...5, id: \.self) { index in
-                  VStack(spacing: 8) {
-                    Text("💸")
-                      .font(.pretendard(size: 28, weight: .bold))
-                      .frame(maxWidth: .infinity)
-                    
-                    Text("생활비")
-                      .font(.pretendard(size: 14, weight: .bold))
-                      .frame(maxWidth: .infinity)
-                  }
-                  .padding(.vertical, 12)
-                  .background(ColorConstants.primary7)
-                  .overlay(
-                    RoundedRectangle(cornerRadius: 4)
-                      .stroke(ColorConstants.primary, lineWidth: 1)
-                  )
+          IGOInputForm {
+            Text("챌린지 주제")
+              .font(.pretendard(size: 18, weight: .bold))
+          } content: {
+            LazyVGrid(columns: Array(repeating: .init(), count: 3), spacing: 16) {
+              ForEach(1...5, id: \.self) { index in
+                VStack(spacing: 8) {
+                  Text("💸")
+                    .font(.pretendard(size: 28, weight: .bold))
+                    .frame(maxWidth: .infinity)
+                  
+                  Text("생활비")
+                    .font(.pretendard(size: 14, weight: .bold))
+                    .frame(maxWidth: .infinity)
                 }
+                .padding(.vertical, 12)
+                .background(ColorConstants.primary7)
+                .overlay(
+                  RoundedRectangle(cornerRadius: 4)
+                    .stroke(ColorConstants.primary, lineWidth: 1)
+                )
               }
             }
           }
           
           // 챌린지 시작일 섹션
-          Section {
-            VStack {
+          IGOInputForm {
+            Text("챌린지 시작일")
+              .font(.pretendard(size: 18, weight: .bold))
+          } content: {
+            Button {
+              
+            } label: {
               HStack {
-                Text("챌린지 시작일")
-                  .font(.pretendard(size: 18, weight: .bold))
+                Image(systemName: "calendar")
+                
+                Text("챌린지 시작일을 선택해주세요.")
                 
                 Spacer()
               }
-              
-              Button {
-                
-              } label: {
-                HStack {
-                  Image(systemName: "calendar")
-                  
-                  Text("챌린지 시작일을 선택해주세요.")
-                  
-                  Spacer()
-                }
-                .font(.pretendard(size: 16, weight: .medium))
-                .padding(.horizontal, 16)
-                .padding(.vertical, 12)
-              }
-              .foregroundColor(ColorConstants.gray3)
-              .buttonStyle(.plain)
-              .background(
-                RoundedRectangle(cornerRadius: 4)
-                  .stroke(ColorConstants.gray3)
-              )
+              .font(.pretendard(size: 16, weight: .medium))
+              .padding(.horizontal, 16)
+              .padding(.vertical, 12)
             }
+            .foregroundColor(ColorConstants.gray3)
+            .buttonStyle(.plain)
+            .background(
+              RoundedRectangle(cornerRadius: 4)
+                .stroke(ColorConstants.gray3)
+            )
           }
           
           // 챌린지 제목 섹션
-          Section {
-            HStack(alignment: .center) {
-              Text("제목")
-                .font(.pretendard(size: 18, weight: .bold))
-              
-              Spacer()
-              
-              Text("최소 5자 / 최대 15자")
-                .font(.pretendard(size: 12, weight: .medium))
-                .foregroundColor(ColorConstants.gray3)
-            }
-            
+          IGOInputForm {
+            Text("제목")
+              .font(.pretendard(size: 18, weight: .bold))
+          } subTitleView: {
+            Text("최소 5자 / 최대 15자")
+              .font(.pretendard(size: 12, weight: .medium))
+              .foregroundColor(ColorConstants.gray3)
+          } content: {
             TextField("제목을 입력해주세요.", text: $title)
               .padding(.horizontal, 16)
               .padding(.vertical, 12)
@@ -149,19 +126,13 @@ struct GenerateRoomScene: View {
           }
           
           // 챌린지 내용 섹션
-          Section {
-            HStack(alignment: .center) {
-              Text("내용")
-                .font(.pretendard(size: 18, weight: .bold))
-              
-              Spacer()
-              
-              Text("최대 50자")
-                .font(.pretendard(size: 12, weight: .medium))
-                .foregroundColor(ColorConstants.gray3)
-            }
-            
-            // TODO: - Custom TextView 구현하기
+          IGOInputForm {
+            Text("내용")
+              .font(.pretendard(size: 18, weight: .bold))
+          } subTitleView: {
+            Text("최대 50자")
+              .font(.pretendard(size: 12, weight: .medium))
+              .foregroundColor(ColorConstants.gray3)
           }
         }
         .padding(.horizontal, 24)
