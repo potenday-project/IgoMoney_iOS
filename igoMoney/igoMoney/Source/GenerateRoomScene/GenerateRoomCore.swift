6dfefe30279af772c4ee5f839f9ba65b52d3e223
미니 <cow970814@naver.com>
2023-10-11 15:34:19 +0900

    feat : 챌린지 방 생성 코어 파일 생성

---
+++ igoMoney/igoMoney/Source/GenerateRoomScene/GenerateRoomCore.swift
@@ -0,0 +1,24 @@
+//
+//  GenerateRoomCore.swift
+//  igoMoney
+//
+//  Copyright (c) 2023 Minii All rights reserved.
+
+import ComposableArchitecture
+
+struct GenerateRoomCore: Reducer {
+  struct State: Equatable {
+    
+  }
+  
+  enum Action: Equatable {
+    
+  }
+  
+  func reduce(into state: inout State, action: Action) -> Effect<Action> {
+    switch action {
+    default:
+      return .none
+    }
+  }
+}
