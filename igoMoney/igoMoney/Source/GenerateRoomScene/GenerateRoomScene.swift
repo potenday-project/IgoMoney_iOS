aac2a2dbae3c94a22151a1debc21be30736141a7
미니 <cow970814@naver.com>
2023-10-11 15:33:19 +0900

    feat : 챌린지 방 생성 화면 파일 생성

---
+++ igoMoney/igoMoney/Source/GenerateRoomScene/GenerateRoomScene.swift
@@ -0,0 +1,19 @@
+//
+//  GenerateRoomScene.swift
+//  igoMoney
+//
+//  Copyright (c) 2023 Minii All rights reserved.
+
+import SwiftUI
+
+struct GenerateRoomScene: View {
+  var body: some View {
+    VStack {
+      Text(String(describing: Self.self))
+    }
+  }
+}
+
+#Preview {
+  GenerateRoomScene()
+}
