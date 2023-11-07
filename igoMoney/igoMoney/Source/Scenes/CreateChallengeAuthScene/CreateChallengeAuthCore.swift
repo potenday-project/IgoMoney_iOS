203e4712c03e8582b81a66522d1d0962555aab56
미니 <cow970814@naver.com>
2023-11-07 20:16:04 +0900

    feat : Challenge 인증 작성 구현

--- igoMoney/igoMoney/Source/Scenes/CreateChallengeAuthScene/CreateChallengeAuthCore.swift
+++ igoMoney/igoMoney/Source/Scenes/CreateChallengeAuthScene/CreateChallengeAuthCore.swift
@@ -9,7 +9,9 @@ import ComposableArchitecture
 struct CreateChallengeAuthCore: Reducer {
   struct State: Equatable {
     let challenge: Challenge
+    var title: String = ""
     var money: String = ""
+    var content: String = ""
     
     init(challenge: Challenge) {
       self.challenge = challenge
@@ -17,14 +19,24 @@ struct CreateChallengeAuthCore: Reducer {
   }
   
   enum Action: Equatable {
+    case titleChanged(String)
     case moneyChanged(String)
+    case contentChanged(String)
   }
   
   func reduce(into state: inout State, action: Action) -> Effect<Action> {
     switch action {
+    case let .titleChanged(title):
+      state.title = title
+      return .none
+      
     case let .moneyChanged(changedString):
       state.money = changedString.map { String($0) }.compactMap { Int($0) }
         .map { String($0) }.joined()
+      return .none
+      
+    case let .contentChanged(content):
+      state.content = content
       return .none
     }
   }
