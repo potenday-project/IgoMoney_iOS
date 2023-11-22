c44132eafdbaf02137f625a392a637f146d8f2a3
미니 <cow970814@naver.com>
2023-11-22 23:40:59 +0900

    feat : NotificationClient 메서드 구현

--- igoMoney/igoMoney/Source/Clients/NotificationClient/NotificationClient + live.swift
+++ igoMoney/igoMoney/Source/Clients/NotificationClient/NotificationClient + live.swift
@@ -9,8 +9,24 @@ import ComposableArchitecture
 
 extension NotificationClient {
   static var liveValue: NotificationClient = .init { userID in
-    <#code#>
+    let api = NotificationAPI(
+      method: .get,
+      path: "/users/notification/\(userID)",
+      query: [:],
+      header: [:]
+    )
+    
+    let response: [Notification] = try await APIClient.request(to: api)
+    
+    return response
   } readNotification: { notificationID in
-    <#code#>
+    let api = NotificationAPI(
+      method: .post,
+      path: "/users/notification/check/\(notificationID)",
+      query: [:],
+      header: [:]
+    )
+    
+    return try await APIClient.execute(to: api)
   }
 }
