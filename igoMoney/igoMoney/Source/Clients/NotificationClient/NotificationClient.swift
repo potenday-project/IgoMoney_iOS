5447b0a486685903bf20e987de3d590d8b19a98b
미니 <cow970814@naver.com>
2023-11-23 15:42:36 +0900

    feat : Notification 화면 구현

--- igoMoney/igoMoney/Source/Clients/NotificationClient/NotificationClient.swift
+++ igoMoney/igoMoney/Source/Clients/NotificationClient/NotificationClient.swift
@@ -7,7 +7,7 @@
 import Foundation
 import Dependencies
 
-struct Notification: Hashable, Decodable {
+struct Notification: Hashable, Decodable, Equatable {
   let ID: Int
   let userID: Int
   let title: String
@@ -19,10 +19,17 @@ struct Notification: Hashable, Decodable {
     case title = "title"
     case content = "message"
   }
+  
+  static let `default` = Notification(
+    ID: 1,
+    userID: 1,
+    title: "챌린지 결과",
+    content: "아이고머니님! 뒷주머니님과의 챌린지 대결에서 승리하셔서 뱃지를 획득하셨습니다. 새로운 챌린지에 도전해보세요."
+  )
 }
 
 struct NotificationClient {
-  var fetchUnreadNotification: @Sendable (_ userID: Int) async throws -> [Notification]
+  var fetchUnreadNotification: @Sendable () async throws -> [Notification]
   var readNotification: @Sendable (_ notificationID: Int) async throws -> Data
 }
 
--- igoMoney/igoMoney/Source/Scenes/NotificationScene/NotificationScene.swift
+++ igoMoney/igoMoney/Source/Scenes/NotificationScene/NotificationScene.swift
@@ -5,13 +5,99 @@
 //  Copyright (c) 2023 Minii All rights reserved.
 
 import SwiftUI
+import ComposableArchitecture
 
 struct NotificationScene: View {
-    var body: some View {
-        Text("Hello, World!")
+  let store: StoreOf<NotificationCore>
+  
+  var body: some View {
+    VStack(spacing: 24) {
+      IGONavigationBar {
+        Text("알림")
+          .font(.pretendard(size: 20, weight: .bold))
+      } leftView: {
+        Button {
+          
+        } label: {
+          Image(systemName: "chevron.backward")
+            .resizable()
+            .scaledToFit()
+            .frame(width: 18, height: 20)
+        }
+        .buttonStyle(.plain)
+      } rightView: {
+        EmptyView()
+      }
+      
+      WithViewStore(store, observe: { $0 }) { viewStore in
+        if viewStore.unreadNotifications.isEmpty {
+          NotificationEmptyView()
+        } else {
+          List(viewStore.unreadNotifications, id: \.ID) { notification in
+            NotificationCell(notification: notification)
+          }
+          .listStyle(.plain)
+        }
+      }
+      
+      Spacer()
     }
+    .padding(.horizontal, 24)
+    .padding(.top, 16)
+    .onAppear {
+      store.send(.onAppear)
+    }
+    .navigationBarHidden(true)
+  }
+}
+
+struct NotificationEmptyView: View {
+  var body: some View {
+    VStack {
+      Image("icon_notification_off")
+      
+      Text("알림이 아직 없어요.")
+        .font(.pretendard(size: 14, weight: .bold))
+    }
+    .frame(maxWidth: .infinity)
+    .padding(.horizontal, 16)
+    .padding(.vertical, 13)
+    .background(Color.white)
+    .cornerRadius(10)
+    .shadow(color: ColorConstants.gray5, radius: 4, y: 2)
+  }
+}
+
+struct NotificationCell: View {
+  let notification: Notification
+  
+  var body: some View {
+    VStack(alignment: .leading, spacing: 4) {
+      HStack {
+        Label(notification.title, image: "icon_bolt")
+          .font(.pretendard(size: 12, weight: .medium))
+          .foregroundColor(ColorConstants.gray)
+        
+        Spacer()
+      }
+      
+      Text(notification.content)
+        .font(.pretendard(size: 14, weight: .bold))
+        .multilineTextAlignment(.leading)
+    }
+    .padding(.vertical, 12)
+    .padding(.horizontal, 16)
+    .background(Color.white)
+    .cornerRadius(10)
+    .shadow(color: ColorConstants.gray5, radius: 4, y: 2)
+  }
 }
 
 #Preview {
-    NotificationScene()
+  NotificationScene(
+    store: Store(
+      initialState: NotificationCore.State(),
+      reducer: { NotificationCore() }
+    )
+  )
 }
