78abd82fb19e9936883ccfd8f8ce9c046c6e4fde
미니 <cow970814@naver.com>
2023-11-27 16:51:07 +0900

    feat : Challenge 둘러보기 데이터 페이지네이션 추가

--- igoMoney/igoMoney/Source/Clients/NetworkClient/RequestGenerator.swift
+++ igoMoney/igoMoney/Source/Clients/NetworkClient/RequestGenerator.swift
@@ -10,7 +10,7 @@ protocol RequestGenerator {
   var baseURL: String { get }
   var method: HTTPMethod { get }
   var path: String { get }
-  var query: [String: Any] { get }
+  var query: [String: String] { get }
   var header: [String: Any] { get }
   var body: HTTPBody? { get }
 }
@@ -19,7 +19,7 @@ extension RequestGenerator {
   func generate() throws -> URLRequest {
     var component = URLComponents(string: baseURL)
     component?.path = path
-    component?.queryItems = query.map { URLQueryItem(name: $0.key, value: $0.value as? String) }
+    component?.queryItems = query.map { URLQueryItem(name: $0.key, value: $0.value) }
     
     guard let url = try? component?.asURL() else {
       throw APIError.didNotConvertRequest
